3b043d4d26cc625962658f6c2860a75900df6545
mahendra-gehlot <connect.mge@gmail.com>
2022-11-10 19:26:03 +0530

    bug-fixed

--- src/model/train.py
+++ src/model/train.py
@@ -143,8 +143,8 @@ def train(model, criterion, optimizer, schedular, num_of_epochs):
             running_loss += images.size(0) * loss.item()
             running_accuracy += torch.sum((pro_predict > 0.0) == labels.data)
 
-            if epoch % 10 == 0:
-                torch.save(model.state_dict(), f'model/infer_model.pt')
+        if epoch % 10 == 0:
+            torch.save(model.state_dict(), f'model/infer_model.pt')
 
         schedular.step()
 
